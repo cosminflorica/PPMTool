ed5050d14c7a8f120453853fe4267808bfdf3aa1
Cosmin Florica <florica.cosmin@yahoo.com>
2020-04-26 13:06:37 +0300

    first commit

---
+++ src/main/java/com/cosminflorica/ppmtool/PpmtoolApplication.java
@@ -0,0 +1,13 @@
+package com.cosminflorica.ppmtool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpmtoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PpmtoolApplication.class, args);
+	}
+
+}
