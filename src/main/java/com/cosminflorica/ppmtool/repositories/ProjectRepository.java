2ea800daa46717b187bf26004a832def47794275
Cosmin Florica <florica.cosmin@yahoo.com>
2020-04-26 13:47:28 +0300

    Project Repository

---
+++ src/main/java/com/cosminflorica/ppmtool/repositories/ProjectRepository.java
@@ -0,0 +1,10 @@
+package com.cosminflorica.ppmtool.repositories;
+
+import com.cosminflorica.ppmtool.domain.Project;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProjectRepository extends CrudRepository<Project, Long> {
+
+}
