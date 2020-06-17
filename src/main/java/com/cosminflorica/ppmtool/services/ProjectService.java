3ba85ab00a6f77977137b5c080a695bb24d3d607
Cosmin Florica <florica.cosmin@yahoo.com>
2020-06-17 16:13:49 +0300

    exception handling

--- src/main/java/com/cosminflorica/ppmtool/services/ProjectService.java
+++ src/main/java/com/cosminflorica/ppmtool/services/ProjectService.java
@@ -1,6 +1,7 @@
 package com.cosminflorica.ppmtool.services;
 
 import com.cosminflorica.ppmtool.domain.Project;
+import com.cosminflorica.ppmtool.exceptions.ProjectIdException;
 import com.cosminflorica.ppmtool.repositories.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,6 +13,12 @@ public class ProjectService {
 
     public Project saveOrUpdateProject(Project project) {
 
-        return projectRepository.save(project);
+        try {
+            project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+            return projectRepository.save(project);
+        } catch (Exception e) {
+            throw new ProjectIdException("Project ID '" + project.getProjectIdentifier().toUpperCase() + "' already exists");
+        }
+
     }
 }
