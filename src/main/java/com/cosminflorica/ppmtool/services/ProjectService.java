feb08533a733cd3976a2dffd50ab615510b8c96e
Cosmin Florica <florica.cosmin@yahoo.com>
2021-01-25 19:55:23 +0100

    delete

--- src/main/java/com/cosminflorica/ppmtool/services/ProjectService.java
+++ src/main/java/com/cosminflorica/ppmtool/services/ProjectService.java
@@ -34,4 +34,12 @@ public class ProjectService {
     public Iterable<Project> findAllProjects(){
         return projectRepository.findAll();
     }
+
+    public void deleteProjectByIdentifier(String projectId){
+        Project project = projectRepository.findByProjectIdentifier(projectId.toUpperCase());
+        if(project==null){
+            throw new ProjectIdException("Project Id "+ projectId+" does not exists");
+        }
+        projectRepository.delete(project);
+    }
 }
