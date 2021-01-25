134731b47b2ff7a9c6639b852120dabc3a71a0a9
Cosmin Florica <florica.cosmin@yahoo.com>
2021-01-25 20:27:21 +0100

    ui

--- src/main/java/com/cosminflorica/ppmtool/services/ProjectService.java
+++ src/main/java/com/cosminflorica/ppmtool/services/ProjectService.java
@@ -26,7 +26,7 @@ public class ProjectService {
     public Project findProjectByIdentifier(String projectId){
         Project project = projectRepository.findByProjectIdentifier(projectId.toUpperCase());
         if(project==null){
-            throw new ProjectIdException("Project Id "+projectId+" does not exists");
+            throw new ProjectIdException("Project Id " + projectId + " does not exists");
         }
         return project;
     }
@@ -38,7 +38,7 @@ public class ProjectService {
     public void deleteProjectByIdentifier(String projectId){
         Project project = projectRepository.findByProjectIdentifier(projectId.toUpperCase());
         if(project==null){
-            throw new ProjectIdException("Project Id "+ projectId+" does not exists");
+            throw new ProjectIdException("Project Id " + projectId + " does not exists");
         }
         projectRepository.delete(project);
     }
